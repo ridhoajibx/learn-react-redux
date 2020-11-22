0c9efa98865f9ce1c29c1fc043692b4fc8a7a3f1
ridhoajibx <ridhoajibx@gmail.com>
2020-11-23 04:40:34 +0700

    back

--- react-redux-axios/src/App.js
+++ react-redux-axios/src/App.js
@@ -1,33 +1,58 @@
-import React, { useState, useMemo } from 'react';
-import ComponentA from './components/ComponentA';
-import ComponentB from './components/ComponentB';
+import React, { useEffect, useReducer } from 'react';
+import axios from 'axios'
+
+import { Badge, ListGroup, ListGroupItem } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-    const [countA, setCountA] = useState(0);
-    const [countB, setCountB] = useState(0);
-    const incrementA = () => {
-        setCountA(countA + 1)
-    }
-    const incrementB = () => {
-        setCountB(countB + 1)
-    }
+const initialState = {
+    loading: true,
+    errors: "",
+    todos: []
+};
 
-    const memoComponentA = useMemo(()=>{
-        return <ComponentA count={ countA } />
-    }, [countA])
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "SET_DATA":
+            return {
+                loading: false,
+                errors: "",
+                todos: action.payload
+            }
+        case "SET_ERRORS":
+            return {
+                loading: false,
+                errors: "There are no data here",
+                todos: []
+            }
+        default:
+            return initialState;
+    }
+}
+
+function App() {
+    const [state, dispatch] = useReducer(reducer, initialState)
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/todos')
+            .then(res => {
+                console.log(res.data);
+                dispatch({ type: 'SET_DATA', payload: res.data })
+            })
+            .catch(err => {
+                dispatch({ type: 'SET_ERRORS' })
+            })
+    }, []);
+
+    const listMarkup = (
+        <ListGroup>
+            { state.todos.map((todo, i) => (
+                <ListGroupItem key={todo.id}>{todo.title} {todo.completed ? (<Badge color="primary">Completed</Badge>) : (<Badge color="danger">UnCompleted</Badge>)}</ListGroupItem>
+            ))}
+        </ListGroup>
+    )
     return (
         <div className="App">
-            App js Count A : { countA }
-            <p>
-                <button onClick={ incrementA }>Increment count A</button>
-            </p>
-            <p>
-                <button onClick={ incrementB }>Increment count B</button>
-            </p>
-            { memoComponentA }
-            <p></p>
-            <ComponentB count={ countB } />
+            { state.loading ? 'Loading' : (state.errors ? state.errors : listMarkup)}
         </div>
     );
 }
